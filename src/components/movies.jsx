e873e442612c197f6a77a20c50b523c5a04a9fdf
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2018-11-08 12:09:50 -0500

    Movies component is created

---
+++ src/components/movies.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { getMovies } from '../services/fake/MovieService'
+
+class Movies extends Component {
+    state = {
+    
+    }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Movies;

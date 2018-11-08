ae99b90739416aa3ccb6d061bef81c3da42e7b7c
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2018-11-08 13:59:53 -0500

    Status is added tho the component

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,11 +1,33 @@
-import React, { Component } from 'react';
-import { getMovies } from '../services/fake/MovieService'
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
 
 class Movies extends Component {
-    state = {}
-    render() { 
-        return (  );
-    }
+  state = {
+    movies: getMovies()
+  };
+
+  render() {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Genre</th>
+            <th>Stock</th>
+            <th>Rate</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td />
+            <td />
+            <td />
+            <td />
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
 }
- 
+
 export default Movies;

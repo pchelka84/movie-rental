e12cd6b979b8700dc2118c3ee45da6ce2dc498b5
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2018-11-08 14:13:30 -0500

    Delete button is added into the table

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -15,6 +15,7 @@ class Movies extends Component {
             <th>Genre</th>
             <th>Stock</th>
             <th>Rate</th>
+            <th />
           </tr>
         </thead>
         <tbody>
@@ -24,6 +25,9 @@ class Movies extends Component {
               <td>{movie.genre.name}</td>
               <td>{movie.numberInStock}</td>
               <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button className="btn btn-danger btn-sm">Delete</button>
+              </td>
             </tr>
           ))}
         </tbody>

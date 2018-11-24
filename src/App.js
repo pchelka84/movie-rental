f7322c43c3aad54b2ee8bc6a4ec25b7579c9cae9
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2018-11-24 15:24:10 -0500

    NavBar is added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./components/movies";
-// import NavBar from "./components/navBar";
 import Rentals from "./components/rentals";
 import Customers from "./components/customers";
+import NavBar from "./components/navBar";
 import NotFound from "./components/notFound";
 import "./App.css";
 
@@ -11,14 +11,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <NavBar /> */}
+        <NavBar />
         <main role="main" className="container">
           <Switch>
             <Route path="/movies" component={Movies} />
             <Route path="/customers" component={Customers} />
             <Route path="/rentals" component={Rentals} />
             <Route path="/not-found" component={NotFound} />
-            <Redirect from="/" to="/movies" />
+            <Redirect from="/" exact to="/movies" />
+            {/* Redirecting from invalid URL + 'exact' above*/}
+            <Redirect to="not-found" />
           </Switch>
         </main>
       </div>

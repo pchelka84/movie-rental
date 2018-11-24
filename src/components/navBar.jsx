b1d78a45a6eb300af6a440aee6196225c0e80ef8
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2018-11-24 15:22:12 -0500

    The NavBar content is added

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -3,12 +3,12 @@ import { Link, NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <Link class="navbar-brand" to="/movies">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
         Vidly
       </Link>
       <button
-        class="navbar-toggler"
+        className="navbar-toggler"
         type="button"
         data-toggle="collapse"
         data-target="#navbarContent"
@@ -16,9 +16,9 @@ const NavBar = () => {
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span class="navbar-toggler-icon" />
+        <span className="navbar-toggler-icon" />
       </button>
-      <div class="collapse navbar-collapse" id="navbarContent">
+      <div className="collapse navbar-collapse" id="navbarContent">
         <div className="navbar-nav">
           <NavLink className="nav-item nav-link" to="/movies">
             Movies

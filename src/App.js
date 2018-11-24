cb509e2d2ec6996fa917ad6f2f15d0a483786700
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2018-11-24 14:57:54 -0500

    Adding Routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
+import { Route, Redirect } from "react-router-dom";
 import Movies from "./components/movies";
-import NavBar from "./components/navBar";
+// import NavBar from "./components/navBar";
+import Rentals from "./components/rentals";
+import Customers from "./components/customers";
+import NotFound from "./components/notFound";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar />
+        {/* <NavBar /> */}
         <main role="main" className="container">
-          <Movies />
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" to="/movies" />
         </main>
       </div>
     );

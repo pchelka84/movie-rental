6ea83a447318bc13913c4159dc117580f79d73f1
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2018-11-24 14:13:45 -0500

    Adding Routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Movies from "./components/movies";
+import NavBar from "./components/navBar";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar />
         <main role="main" className="container">
           <Movies />
         </main>

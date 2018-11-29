63b21cf0543e0156016140ba54f0c5a06fec93a0
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2018-11-28 21:36:22 -0500

    Handling inout and password inputs

--- src/components/common/loginForm.jsx
+++ src/components/common/loginForm.jsx
@@ -16,11 +16,12 @@ class LoginForm extends Component {
 
   handleChange = e => {
     const account = { ...this.state.account };
-    account.username = e.currentTarget.value;
+    account[e.currentTarget.name] = e.currentTarget.value;
     this.setState({ account: account });
   };
 
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -28,17 +29,25 @@ class LoginForm extends Component {
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input
-              value={this.state.account.username}
+              value={account.username}
               onChange={this.handleChange}
               autoFocus
               id="username"
+              name="username"
               type="text"
               className="form-control"
             />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              value={account.password}
+              onChange={this.handleChange}
+              name="password"
+              id="password"
+              type="text"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>

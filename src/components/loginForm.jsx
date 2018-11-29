3395d46bb8cf853a248f6b6804bdf116be00318c
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2018-11-29 11:54:55 -0500

    Moving LoginForm out of 'common' folder

---
+++ src/components/loginForm.jsx
@@ -0,0 +1,96 @@
+import React, { Component } from "react";
+import Joi from "joi-browser";
+import Input from "./common/input";
+
+class LoginForm extends Component {
+  state = {
+    account: { username: "", password: "" },
+    errors: {}
+  };
+
+  schema = {
+    username: Joi.string()
+      .required()
+      .label("Username"),
+    password: Joi.string()
+      .required()
+      .label("Password")
+  };
+
+  // Validating the entire form
+  validate = () => {
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+
+    if (!error) return null;
+
+    const errors = {};
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
+  };
+
+  handleSubmit = e => {
+    // prevents submittin gform to a server
+    // which causes the full page reload
+    e.preventDefault();
+
+    const errors = this.validate();
+    console.log(errors);
+    // erros should never be null
+    this.setState({ errors: errors || {} });
+    if (errors) return;
+
+    // call the server
+    console.log("Submitted");
+  };
+
+  // Validating only one input
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMeassage = this.validateProperty(input);
+    if (errorMeassage) errors[input.name] = errorMeassage;
+    else delete errors[input.name];
+
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+
+    this.setState({ account: account, errors });
+  };
+
+  render() {
+    const { account, errors } = this.state;
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          <Input
+            name="username"
+            value={account.username}
+            label="Username"
+            onChange={this.handleChange}
+            error={errors.username}
+          />
+          <Input
+            name="password"
+            value={account.password}
+            label="Password"
+            onChange={this.handleChange}
+            error={errors.password}
+          />
+          <button disabled={this.validate()} className="btn btn-primary">
+            Login
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
